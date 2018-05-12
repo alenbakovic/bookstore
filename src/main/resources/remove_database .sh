cda4360e1ed42cb8473c4f99e7112095727f223e
Alen Bakovic <alen.bakovic0@gmail.com>
2018-05-12 19:54:24 +0200

    Manual for starting app added.

--- src/main/resources/remove_database .sh
+++ src/main/resources/remove_database .sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+printf "REMOVING bookstore DATABASE\n"
+sudo -u postgres psql -c "DROP DATABASE bookstore;"
+
 printf "REMOVING bookstore ROLE\n"
 sudo -u postgres psql -c "DROP USER bookstore;"
 
-printf "REMOVING bookstore DATABASE\n"
-sudo -u postgres psql -c "DROP DATABASE bookstore;"
+
 
